7ea12b3f5923bc43802182fe60cf15347626ac55
TnJUFI <xtrgaming233@gmail.com>
2025-01-16 21:22:43 +0700

    task-action

--- src/features/tasks/components/task-actions.tsx
+++ src/features/tasks/components/task-actions.tsx
@@ -12,6 +12,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 import { useDeleteTask } from "../api/use-delete-task";
+import { useEditTaskModal } from "../hooks/use-edit-task-modal";
 
 interface TaskActionsProps {
     id: string;
@@ -22,6 +23,8 @@ interface TaskActionsProps {
 export const TaskActions = ({ id, projectId, children }: TaskActionsProps) => {
     const workspaceId = useWorkspaceId();
     const router = useRouter();
+
+    const { open } = useEditTaskModal();
 
     const [ConfirmDialog, confirm] = useConfirm(
         "Delete task",
@@ -68,7 +71,7 @@ export const TaskActions = ({ id, projectId, children }: TaskActionsProps) => {
                         Open Project
                     </DropdownMenuItem>
                     <DropdownMenuItem
-                        onClick={() => {}}
+                        onClick={() => open(id)}
                         className="font-medium p-[10px]"
                     >
                         <PencilIcon className="size-4 mr-2 stroke-2" />
